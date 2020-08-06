d51ff8a317977523bbaf67e457dbae1530a7be9d
pochi-gh <pochigm@gmail.com>
2020-08-06 21:25:18 +0900

    ストロングパラメーターの設定

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,11 +1,13 @@
 class TicketsController < ApplicationController
 
   def index
-    @tickets = Ticket.where(live_id: 2)
-    @artist = Artist.find(1)
+    @tickets = Ticket.where(live_id: artist_params[:live_id])
+    @artist = Artist.find(artist_params[:artist_id])
   end
 
-
-
+  private
+    def artist_params
+      params.permit(:artist_id,:live_id)
+    end
 
 end
