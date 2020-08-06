4bf79442b7da682921039733a2121b70aa43919d
pochi-gh <pochigm@gmail.com>
2020-08-06 21:06:57 +0900

    link_to　prams設定

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,8 +1,11 @@
 class TicketsController < ApplicationController
 
   def index
-    @tickets = Ticket.where(live_id: 1)
+    @tickets = Ticket.where(live_id: 2)
+    @artist = Artist.find(1)
   end
+
+
 
 
 end
