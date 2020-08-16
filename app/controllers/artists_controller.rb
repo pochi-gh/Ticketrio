3720d9815929b01ed51597df817040bd8f3acf40
pochi-gh <pochigm@gmail.com>
2020-08-16 13:41:32 +0900

    アーティスト名のユニーク、空欄のバリテーション追加

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -11,4 +11,19 @@ class ArtistsController < ApplicationController
   def new
     @artist = Artist.new
   end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to "/artists"
+    else
+      render :new
+    end
+  end
+
+  private
+  def artist_params
+    params.require(:artist).permit(:artist_name)
+  end
+
 end
