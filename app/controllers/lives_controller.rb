7bf6593a0cd45241576566347ab990bd5caeedaa
pochi-gh <pochigm@gmail.com>
2020-08-17 17:04:48 +0900

    new_viewの作成、フォームの作成

--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -4,9 +4,28 @@ class LivesController < ApplicationController
   end
 
 
+  def new
+    # @artist = Artist.find(artist_params[:id])
+    @live = Live.new
+
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to "/artists"
+    else
+      render :new
+    end
+  end
 
   private
   def artist_params
     params.permit(:id)
   end
+
+  def live_params
+
+  end
+
 end
