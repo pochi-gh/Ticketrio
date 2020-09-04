05df6a782a38a649dab7ea7da7b8b113412bf4ce
pochi-gh <pochigm@gmail.com>
2020-09-05 00:12:31 +0900

    ヘッダーの修正

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -11,7 +11,7 @@ class TicketsController < ApplicationController
     @artist = Artist.find(artist_live_params[:artist_id])
     @live = Live.find(artist_live_params[:live_id])
     @ticket = Ticket.new
-    @lives = ArtistConcert.where(artist_id: artist_live_params[:artist_id]).includes(:live).order("lives.data ASC")
+    @lives = ArtistConcert.where(artist_id: artist_live_params[:artist_id]).includes(:live).where('data >= ?', Date.today).order("lives.data ASC")
   end
 
   def create
