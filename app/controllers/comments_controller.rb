84cb1aedeeff8c5cb35506725ca206421c678772
pochi-gh <pochigm@gmail.com>
2020-09-17 20:11:14 +0900

    コメント周りのバリデーション及びエラーハンドリング

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,43 @@
 class CommentsController < ApplicationController
+
   def create
-      Comment.create(comment_params)
-      redirect_to "/tickets/#{comment_params[:ticket_id]}"
+      comment = Comment.create(comment_params)
+      if comment.save
+        flash[:notice] = 'コメントしました。'
+        redirect_to "/tickets/#{comment_params[:ticket_id]}"
+      else
+        flash[:alert] = 'コメントを入力してください。'
+        redirect_to "/tickets/#{comment_params[:ticket_id]}"
+      end
   end
+
+  def edit
+    @ticket = Ticket.find(params[:ticket_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    comment = Comment.find(params[:id])
+    if comment.update(comment_edit)
+      flash[:notice] = 'コメントを修正しました。'
+      redirect_to "/tickets/#{params[:ticket_id]}"
+    else
+      @ticket = Ticket.find(params[:ticket_id])
+      @comment = Comment.find(params[:id])
+      flash.now[:alert] = '更新できませんでした。コメントを入力してください。'
+      render :edit
+    end
+  end
+
+
 
   private
     def comment_params
       params.require(:comment).permit(:text,:ticket_id).merge(user_id: current_user.id)
     end
+ 
+    def comment_edit
+      params.require(:comment).permit(:text)
+    end
+
 end
